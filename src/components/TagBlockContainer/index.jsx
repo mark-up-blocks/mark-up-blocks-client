90ccf79e8499dc5e0bc42c89484ac4c1d901c5be
Hyun Jo <hay.hyunjo@gmail.com>
2021-10-03 21:44:15 +0900

    Add: TagBlockContainer component

---
+++ src/components/TagBlockContainer/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+
+import TagBlock from "../TagBlock";
+
+const ContainerWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 10px;
+  justify-content: center;
+  align-items: center;
+  border: ${({ theme }) => theme.border.page};
+`;
+
+function TagBlockContainer() {
+  const { tagBlocks } = useSelector((state) => state.challenge);
+
+  return (
+    <ContainerWrapper>
+      {tagBlocks.map(({ _id, block, hasUsed }) => (
+        !hasUsed && <TagBlock key={_id} _id={_id} block={block} />
+      ))}
+    </ContainerWrapper>
+  );
+}
+
+export default TagBlockContainer;
