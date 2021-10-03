462ee7dd1d9b3e4e50afad54cdb43755004f2057
Hyun Jo <hay.hyunjo@gmail.com>
2021-10-03 21:43:52 +0900

    Add: addChildTree action in challenge reducer

--- src/features/challenge.js
+++ src/features/challenge.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { findBlockTreeById, findBlockTree, findTagBlockById } from "../utils/selectData";
 
 export const challengeSlice = createSlice({
   name: "challenge",
@@ -16,13 +17,35 @@ export const challengeSlice = createSlice({
 
       return {
         title,
-        tagBlocks,
+        tagBlocks: tagBlocks.map((child) => ({ ...child, childTrees: [], hasUsed: false })),
         boilerplate,
         answer,
       };
     },
+    addChildTree(state, { payload }) {
+      const { containerId, itemId } = payload;
+      const prevContainer = findBlockTree(
+        state.boilerplate, (block) => findTagBlockById(block.childTrees, itemId),
+      );
+
+      if (prevContainer) {
+        const tagBlock = findBlockTreeById(state.boilerplate, itemId);
+        const container = findBlockTreeById(state.boilerplate, containerId);
+
+        prevContainer.childTrees = prevContainer.childTrees.filter(
+          (child) => child._id !== itemId,
+        );
+        container.childTrees.push(tagBlock);
+      } else {
+        const container = findBlockTreeById(state.boilerplate, containerId);
+        const tagBlock = findTagBlockById(state.tagBlocks, itemId);
+
+        container.childTrees.push({ ...tagBlock, _id: itemId });
+        tagBlock.hasUsed = true;
+      }
+    },
   },
 });
 
-export const { setChallenge } = challengeSlice.actions;
+export const { setChallenge, addChildTree } = challengeSlice.actions;
 export default challengeSlice.reducer;
