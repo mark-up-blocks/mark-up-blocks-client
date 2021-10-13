eca1c17e86a940732a61c6018624f41def0e4e43
Hyun Jo <hay.hyunjo@gmail.com>
2021-10-14 00:18:35 +0900

    Modify: Tutorial guide text css style

--- src/components/Tutorial/index.jsx
+++ src/components/Tutorial/index.jsx
@@ -28,9 +28,14 @@ function Tutorial({ onFinish, notifyError }) {
 
   return (
     <div>
-      {isCompleted ? (
-        <FinishPopup onClick={handleFinish} />
-      ) : <div>Mark Up Blocks에 오신 것을 환영합니다! 아래 태그 블록을 div 안으로 옮겨볼까요?</div>}
+      {isCompleted
+        ? <FinishPopup onClick={handleFinish} />
+        : (
+          <Guide>
+            <p className="welcome">Mark Up Blocks에 오신 것을 환영합니다!</p>
+            <p>아래 태그 블록을 div 안으로 옮겨볼까요?</p>
+          </Guide>
+        )}
       <WideDndInterface
         tagBlockContainer={tagBlockContainer}
         boilerplate={boilerplate}
@@ -50,4 +55,15 @@ export default Tutorial;
 const WideDndInterface = styled(DndInterface)`
   width: 100%;
   height: 100%;
+`;
+
+const Guide = styled.div`
+  margin: 10px;
+  padding: 10px;
+  text-align: center;
+
+  .welcome {
+    margin: 10px;
+    font-size: 0.9rem;
+  }
 `;
